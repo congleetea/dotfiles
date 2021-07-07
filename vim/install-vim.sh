fe1194a24ff3af7943b8646958bb42d2b8008c0f
李选聪 <870880194@qq.com>
2021-07-07 16:18:16 +0800

    update vim

--- vim/install-vim.sh
+++ vim/install-vim.sh
@@ -16,7 +16,7 @@ ln -s $PWD/vim $HOME/.vim
 echo ">>> download plug-vim >>>>"
 if [ ! -f $HOME/.vim/autoload/plug.vim ];then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi 
+fi
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ">>>Please type PlugInstall in vim to install plugins>>>>"
